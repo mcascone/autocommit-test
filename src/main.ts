939ee3f6b5149a537c4c19db85bc89e049eefaec
Max Cascone <maxcascone@gmail.com>
2024-08-23 14:51:12 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as fs from 'fs';
+
+async function run(): Promise<void> {
+  try {
+    const readmePath = './README.md';
+    if (fs.existsSync(readmePath)) {
+      core.info('README.md exists');
+    } else {
+      core.setFailed('README.md does not exist');
+    }
+  } catch (error) {
+    core.setFailed(`Action failed with error ${error}`);
+  }
+}
+
+run();
