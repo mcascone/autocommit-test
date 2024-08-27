3907b9bcca15deca45f62c57ee8620965c8ff83d
Max Cascone <max@bitovi.com>
2024-08-27 11:39:19 -0500

    test a changed dist file - should be overwritten

--- dist/main.js
+++ dist/main.js
@@ -39,7 +39,7 @@ function run() {
         try {
             const readmePath = './README.md';
             if (fs.existsSync(readmePath)) {
-                core.info('README.md is found');
+                core.info('README.md THIS SHOULD NOT BE IN THE BUILD');
             }
             else {
                 core.setFailed('README.md does not exist');
