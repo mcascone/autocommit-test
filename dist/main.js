cb175c7b7624b9a5d2052947e608eeca0fe424e1
mcascone <mcascone@users.noreply.github.com>
2024-08-27 16:02:50 +0000

    dist folder autobuild

--- dist/main.js
+++ dist/main.js
@@ -39,7 +39,7 @@ function run() {
         try {
             const readmePath = './README.md';
             if (fs.existsSync(readmePath)) {
-                core.info('README.md is there');
+                core.info('README.md is found');
             }
             else {
                 core.setFailed('README.md does not exist');
